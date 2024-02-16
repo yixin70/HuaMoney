9682211963840c21c79b5299559dfb3e80924a82
Yixin <yixin875@gmail.com>
2024-02-16 17:04:22 +0100

    Added Template Banks

---
+++ HuaMoney/Migrations/20240216155434_Add_Banks.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HuaMoney.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Banks : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Banks (Country, Name)" +
+                                " VALUES ('ES', 'Banco Bilbao Vizcaya Argentaria, S.A'), ('ES', 'Banco de Sabadell, S.A.'), ('ES', 'Santander'), ('ES', 'CaixaBank')" +
+                                " , ('GB', 'Revolut Ltd'), ('ES', 'N26 Bank AG'), ('CN', '上海银行'), ('CN', '微信支付'), ('CN', '支付宝')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"DELETE FROM Banks");
+        }
+    }
+}
