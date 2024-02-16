19b3fc769a5c56d89ad556eec2332071bdc4040a
Yixin <yixin875@gmail.com>
2024-02-16 18:04:09 +0100

    Fix Bank ViewModel

--- HuaMoney/ViewModel/Bank/BankIndexViewModel.cs
+++ HuaMoney/ViewModel/Bank/BankIndexViewModel.cs
@@ -4,10 +4,8 @@ namespace HuaMoney.ViewModel.Bank;
 
 public class BankIndexViewModel
 {
-    public BankIndexViewModel(List<BankDto> bankList)
-    {
-        bankList = bankList;
+    public BankIndexViewModel(List<BankDto> bankDto) {
+        this.bankList = bankDto;
     }
-    
     public List<BankDto> bankList = new List<BankDto>();
 }
